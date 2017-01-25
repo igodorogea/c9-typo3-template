bffc091411fbed23daa0938064d72c6125cd5ae0
Ivan Godorogea <igodorogea@gmail.com>
2017-01-25 21:19:51 +0000

    Add introduction package

--- init.sh
+++ init.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
-cd /
-sudo apt-get purge php.*
+function say {
+    printf "\n--------------------------------------------------------\n"
+    printf "\t$1"
+    printf "\n--------------------------------------------------------\n"
+}
 
-sudo apt-get install python-software-properties
+say "Prepare machine..."
+
+say "start db"
+mysql-ctl start
+
+say "remove existing php"
+cd /
+sudo apt-get purge -y php.*
+
+say "install php 7.1"
+sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
-sudo apt-get install -y php7.1 php7.1-dev php7.1-soap php7.1-xml php7.1-zip php7.1-gd php7.1-mysql php7.1-curl php7.1-mbstring php7.1-bcmath
-
-sudo apt-get install php-pear
+sudo apt-get install -y php7.1 php7.1-dev php7.1-soap php7.1-xml php7.1-zip php7.1-gd php7.1-mysql php7.1-curl php7.1-mbstring php7.1-bcmath php-pear
 sudo pecl install apcu
 
-sudo apt-get autoremove
-sudo apt-get autoclean
+say "Install imagemagick"
+sudo apt-get install -y imagemagick php-imagick
+
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
+
+say "update vhost"
+sudo sed -i 's/\/home\/ubuntu\/workspace$/\/home\/ubuntu\/workspace\/web/' /etc/apache2/sites-available/001-cloud9.conf
+sudo sed -i 's/\/home\/ubuntu\/workspace>$/\/home\/ubuntu\/workspace\/web>/' /etc/apache2/sites-available/001-cloud9.conf
+
+say "Install dependencies"
+cd ~/workspace
+composer install
+
+say "Done."
