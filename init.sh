63487795787793c65aabfc0bb69ce2bca77c8193
Ivan <igodorogea@gmail.com>
2017-02-14 10:56:34 +0200

    install missing apache modules

--- init.sh
+++ init.sh
@@ -32,6 +32,9 @@ sudo apt-get -qq update
 sudo apt-get -qq install -y libapache2-mod-php7.1 php7.1 php7.1-dev php7.1-fpm php7.1-cli php7.1-common php7.1-soap php7.1-xml php7.1-zip php7.1-mbstring php7.1-bcmath php7.1-curl php7.1-gd php7.1-intl php7.1-json php7.1-mcrypt php7.1-mysql php7.1-readline php7.1-sqlite3 php7.1-tidy php7.1-xmlrpc php7.1-xsl
 sudo echo '' | sudo pecl -q install apcu
 
+say "install missing apache modules"
+sudo a2enmod expires headers
+
 
 say "Install imagemagick"
 
